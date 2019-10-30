26ec9f5d43ca2d15f74edf2598e92d53e25376c5
José Manuel Castellano Domínguez <jmcastellano@ingenia.es>
2019-10-30 11:39:52 +0100

    Correcciones Olvido

--- Codigo/Backend/api/login/recuperarcuenta.php
+++ Codigo/Backend/api/login/recuperarcuenta.php
@@ -22,8 +22,9 @@ catch(Exception $e){
 
 $query = "SELECT idusuario FROM usuarioxtoken WHERE token='".$_GET['token']."' ";
 $queryst = $db->prepare($query);
-if(!$queryst->execute()){
-    http_response_code(400);
+$queryst->execute();
+if($queryst->rowCount() <= 0){
+	http_response_code(400);
     exit();
 }
 
@@ -33,22 +34,21 @@ if(!$queryst->execute()){
 
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="UTF-8">
     <title>Recuperación de camiseta HealthShirt</title>
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css" >
-    <link rel="stylesheet" href="../assets/css/bootstrap-theme.min.css" >
-    <script src="../assets/js/bootstrap.min.js" ></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body>
-    <div style="width:100%; text-align:center">
+<body style="padding-top:10%;background: url('../assets/images/fondo.png');">
+    <div class="container" style="text-align:center">
         <img src="../assets/images/icon.png" width="30%" alt="Logo HealtShirt" />
         <h1>HealthShirt</h1>
         <p>Para recuperar la cuenta debe resetear la contraseña, por favor introduce la nueva contraseña debajo</p>
         <form action="nuevacontrasena.php" method="post">
-            <div style="display:inline-block; width:40%; margin-top:1em"><div style="width:30%;text-align:left;display:inline-block">Contraseña:</div><div style="width:60%;text-align:left;display:inline-block"><input type="password" id="password123" name="password123"></div></div><br />
-            <div style="display:inline-block; width:40%; margin-top:1em"><div style="width:30%;text-align:left;display:inline-block">Repetir Contraseña:</div><div style="width:60%;text-align:left;display:inline-block"><input type="password" id="rpassword123" name="rpassword123"></div><br />
+			<input type="password" class="form-control" placeholder="Password" aria-label="Password" name="password123" aria-describedby="password123"><br/>
+			<input type="password" class="form-control" placeholder="Confirma password" aria-label="Confirmar Password" name="rpassword123" aria-describedby="rpassword123"><br/>
             <input type="hidden" name="token" id="token" value="<?php echo $_GET['token']; ?>" />
-            <input style="width:40%; margin-top:1em" type="submit" value="Cambiar clave">
+            <button class="btn btn-primary" type="submit">Cambiar clave</button>
         </form>
     </div>
 </body>
