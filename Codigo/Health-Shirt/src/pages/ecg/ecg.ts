c686ffbc99ca6280618b3394b21a4eaff08eb993
José Manuel Castellano Domínguez <jmcastellano@ingenia.es>
2019-11-07 12:59:48 +0100

    Avance Ventana constantes, Falta Backend

--- Codigo/Health-Shirt/src/pages/ecg/ecg.ts
+++ Codigo/Health-Shirt/src/pages/ecg/ecg.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ConstantesPage } from '@pages/constantes/constantes';
+import {Platform} from 'ionic-angular';
 
 /**
  * Generated class for the EcgPage page.
@@ -16,14 +17,79 @@ import { ConstantesPage } from '@pages/constantes/constantes';
 export class EcgPage {
 
   private constantes:ConstantesPage;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private canvas: any;
+  private ctx: any;
+  data: Array<any>;
+  dataMedia: Array<any>;
+  frame: number;
+  x: number;
+  panAtX: number;
+  continuarAnimacion: boolean;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform) {
     this.constantes = this.navParams.get('constantes');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EcgPage');
     console.log(this.constantes);
+    this.data = [];
+    this.dataMedia=[];
+    this.canvas = document.getElementById("canvasecg");
+    this.ctx = this.canvas.getContext("2d");
+
+    this.canvas.width = this.platform.width();
+    this.canvas.height = this.platform.width();
+
+    //TESTING, esto es a modo de prueba mientras se termina el backend
+    for (var i = 0; i < 50000; i++) {
+      this.data.push(Math.sin(i / 10) * 70 + (this.canvas.height/2));
+    }
+
+    //metemos un primer valor de pulsaciones medias a modo de Test
+    this.dataMedia.push(Math.floor(Math.random() * 51) + 60 );
+    this.frame=0;
+    
+    console.log(this.data);
+    this.x = 0;
+    this.panAtX = 250;
+    this.continuarAnimacion = true;
+    this.animate();
   }
+
+  animate() {
+    if(this.frame > 100){
+      this.frame = 0;
+      this.dataMedia.push(Math.floor(Math.random() * 51) + 60 );
+      //calculamos la media con lo que hay
+      const average = lista => lista.reduce((prev, curr) => prev + curr) / lista.length;
+      this.constantes.setPulsacionesmedias(Math.floor(average(this.dataMedia)));
+    }
+    else{
+      this.frame = this.frame + 1;
+    }
+    this.ctx.fillStyle="#FF0000";
+    if (this.x > this.data.length - 1) {
+        return;
+    }
+
+    if (this.continuarAnimacion) {
+      requestAnimationFrame(this.animate.bind(this));
+    }
+
+    if (this.x++ < this.panAtX) {
+        this.ctx.fillRect(this.x, this.data[this.x], 1, 1);
+
+    } else {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        for (var xx = 0; xx < this.panAtX; xx++) {
+            var y = this.data[this.x - this.panAtX + xx];
+            this.ctx.fillRect(xx, y, 1, 1)
+        }
+    }
+}
+
 
   activarDesactivar(){
     this.constantes.deHabilitado = this.constantes.actual;
--- Codigo/Health-Shirt/src/pages/eda/eda.ts
+++ Codigo/Health-Shirt/src/pages/eda/eda.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { ConstantesPage } from '@pages/constantes/constantes';
+import {Platform} from 'ionic-angular';
 
 /**
  * Generated class for the EdaPage page.
@@ -8,18 +10,99 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
 @Component({
   selector: 'page-eda',
   templateUrl: 'eda.html',
 })
 export class EdaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private constantes:ConstantesPage;
+  private canvas: any;
+  private ctx: any;
+  data: Array<any>;
+  xAxis: Array<any>;
+  private datos = 100;
+  private columnSize = 30;
+  private margin = 10;
+  private Val_max = 700;
+  private Val_min = 100;
+  private rowSize = 25;
+  private stepSize = 100;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform) {
+    this.constantes = this.navParams.get('constantes');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EdaPage');
+    console.log(this.constantes);
+    this.data=[];
+    this.xAxis=[];
+    //cogemos la fecha actual y le restamos 100 minutos
+    var actual = new Date();
+    var milisecondsactual = actual.valueOf();
+    var inicial = new Date(milisecondsactual - 100 * 60000);
+
+    //TESTING rellenamos con datos aleatorios, con los últimos 100 detectados
+    for (var i = 0; i < 100; i++) {
+      this.data.push(Math.floor(Math.random() * 600) + 100 );
+      this.xAxis.push(inicial.toLocaleString());
+      inicial = new Date(milisecondsactual - (100 - i + 1)*60000);
+    }
+    console.log(this.xAxis);
+    this.canvas = document.getElementById("canvaseda");
+    this.ctx = this.canvas.getContext("2d");
+
+    this.canvas.width = this.platform.width();
+    this.canvas.height = this.platform.width();
+    this.dibujarGrafica();
+  }
+
+  activarDesactivar(){
+    this.constantes.deHabilitado = this.constantes.actual;
+    this.constantes.hastaHabilitado = this.constantes.actual;
+  }
+
+  dibujarGrafica() {
+    this.ctx.fillStyle = "#0099ff";
+    this.ctx.font = "20 pt Verdana";
+    var yScale = (this.canvas.height - this.columnSize - this.margin) / (this.Val_max - this.Val_min);
+    var xScale = (this.canvas.width - this.rowSize) / this.datos;
+    
+    this.ctx.strokeStyle="#488aff"; // color of grid lines
+	  this.ctx.beginPath();
+		// print Parameters on X axis, and grid lines on the graph
+	  for (var i=1;i<=this.datos;i=i+35) {
+      var x = i * xScale;
+		  this.ctx.fillText(this.xAxis[i], x,this.columnSize - this.margin);
+	  }
+		// print row header and draw horizontal grid lines
+	  var count =  0;
+	  for (var scale=this.Val_max;scale>=this.Val_min;scale = scale - this.stepSize) {
+		  var y = this.columnSize + (yScale * count * this.stepSize); 
+		  this.ctx.fillText(scale, this.margin,y + this.margin);
+		  this.ctx.moveTo(this.rowSize,y)
+		  this.ctx.lineTo(this.canvas.width,y)
+		  count++;
+	  }
+	  this.ctx.stroke();
+	
+	  this.ctx.translate(this.rowSize,this.canvas.height + this.Val_min * yScale);
+	  this.ctx.scale(1,-1 * yScale);
+	
+		// Color of each dataplot items
+		
+	  this.ctx.strokeStyle="#FF0066";
+	  this.ctx.beginPath();
+    this.ctx.moveTo(0, this.data[0]);
+    for (i=1;i<this.datos;i++) {
+      this.ctx.lineTo(i * xScale, this.data[i]);
+    }
+    this.ctx.stroke();
+
+    //calculamos la media
+    const average = lista => lista.reduce((prev, curr) => prev + curr) / lista.length;
+    this.constantes.setEdamedias(Math.floor(average(this.data)));
   }
 
 }
