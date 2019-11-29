1bc8100eb9baa96e2df307be1d77ae6fbb7a93a6
José Manuel Castellano Domínguez <jmcastellano@ingenia.es>
2019-11-29 14:46:17 +0100

    Haciendo generador de codigo

---
+++ Codigo/Backend/api/generador/generaDatos.php
@@ -0,0 +1,9 @@
+<?php
+// obtenemos el pool de conexiones
+include_once '../config/database.php';
+// obtenemos donde esta la clase Informacion
+include_once '../modelo/informacion.php';
+// codigos de respuesta
+include_once '../modelo/codigos.php';
+//utilidades del sistema
+include_once '../utilidades/utils.php';
