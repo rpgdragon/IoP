877db2341cb3436cf0f748cd6bd7a3bc1326ec7b
José Manuel Castellano Domínguez <jmcastellano@ingenia.es>
2019-11-29 14:47:26 +0100

    Fichero scripts

--- Codigo/Backend/api/generador/generaDatos.php
+++ Codigo/Backend/api/generador/generaDatos.php
@@ -3,7 +3,12 @@
 include_once '../config/database.php';
 // obtenemos donde esta la clase Informacion
 include_once '../modelo/informacion.php';
-// codigos de respuesta
-include_once '../modelo/codigos.php';
 //utilidades del sistema
 include_once '../utilidades/utils.php';
+
+//Este script se va a ejecutar 1 vez por minuto.
+//Va a obtener el minuto actual (sin segundos) y se va a a generar una serie de valores aleatorios
+//y se le va a indicar la fecha dentro del último minuto (pero no hay espera continua, se indica la fecha)
+// En total se insertaran 120 valores en la ejecución de este programa (uno cada 500ms)
+
+//primero obtenemos exactamente en que fecha 
