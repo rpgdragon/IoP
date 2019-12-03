ac2671c5c135d52aa0220a0a742b519ac4aff043
José Manuel Castellano Domínguez <jmcastellano@ingenia.es>
2019-12-03 17:34:29 +0100

    Subiendo versión con autogeneracion de datos

--- Iteraciones/Sprint 4/Scripts/02-Crear Tabla Informacion.sql
+++ Iteraciones/Sprint 4/Scripts/02-Crear Tabla Informacion.sql
@@ -1,3 +1,1 @@
-CREATE TABLE `dbs200017`.`informacion` ( `ecg` INT NOT NULL , `eda` INT NOT NULL , `temperatura` FLOAT NOT NULL , `X` FLOAT NOT NULL , `Y` FLOAT NOT NULL , `Z` FLOAT NOT NULL , `fecha` DATETIME NOT NULL , `numeroserie` INT NOT NULL , PRIMARY KEY (`fecha`, `numeroserie`)) ENGINE = InnoDB PARTITION BY HASH (MONTH(fecha)) PARTITIONS 12;
-
-ALTER TABLE `informacion` CHANGE `numeroserie` `numeroserie` VARCHAR(16) NOT NULL;
+CREATE TABLE `dbs200017`.`informacion` ( `ecg` TEXT , `eda` TEXT , `temperatura` FLOAT NOT NULL , `fecha` DATETIME NOT NULL , `numeroserie` VARCHAR(16) NOT NULL , `bateria` INT NOT NULL, PRIMARY KEY (`fecha`, `numeroserie`)) ENGINE = InnoDB PARTITION BY HASH (DAY(fecha)) PARTITIONS 365;
