53212810aaa24af04317df18731c8cec1b8dde2a
José Manuel Castellano Domínguez <jmcastellano@ingenia.es>
2019-12-04 17:59:48 +0100

    Trabajando en la visualizacion constantes vitales

--- Codigo/Health-Shirt/src/pages/ecg/ecg.ts
+++ Codigo/Health-Shirt/src/pages/ecg/ecg.ts
@@ -20,11 +20,14 @@ export class EcgPage {
   private canvas: any;
   private ctx: any;
   data: Array<any>;
-  dataMedia: Array<any>;
   frame: number;
   x: number;
   panAtX: number;
   continuarAnimacion: boolean;
+  fpsIntervalo: number;
+  fps:number = 20;
+  then: any;
+
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform) {
@@ -33,61 +36,63 @@ export class EcgPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EcgPage');
-    console.log(this.constantes);
     this.data = [];
-    this.dataMedia=[];
     this.canvas = document.getElementById("canvasecg");
     this.ctx = this.canvas.getContext("2d");
 
     this.canvas.width = this.platform.width();
     this.canvas.height = this.platform.width();
 
-    //TESTING, esto es a modo de prueba mientras se termina el backend
-    for (var i = 0; i < 50000; i++) {
-      this.data.push(Math.sin(i / 10) * 70 + (this.canvas.height/2));
-    }
-
-    //metemos un primer valor de pulsaciones medias a modo de Test
-    this.dataMedia.push(Math.floor(Math.random() * 51) + 60 );
     this.frame=0;
     
-    console.log(this.data);
     this.x = 0;
     this.panAtX = 250;
     this.continuarAnimacion = true;
+    this.fpsIntervalo = 1000 / this.fps;
+    this.then = Date.now();
     this.animate();
   }
 
   animate() {
-    if(this.frame > 100){
-      this.frame = 0;
-      this.dataMedia.push(Math.floor(Math.random() * 51) + 60 );
-      //calculamos la media con lo que hay
-      const average = lista => lista.reduce((prev, curr) => prev + curr) / lista.length;
-      this.constantes.setPulsacionesmedias(Math.floor(average(this.dataMedia)));
-    }
-    else{
-      this.frame = this.frame + 1;
-    }
-    this.ctx.fillStyle="#FF0000";
-    if (this.x > this.data.length - 1) {
-        return;
+    //lo primero es comprobar si hay nuevos datos
+    if(this.constantes.getValorecgcambiado()==true){
+      this.data = this.constantes.getDatosecg();
+      this.constantes.setValorecgcambiado(false);
+      this.x = 0;
+      this.then = Date.now();
     }
 
     if (this.continuarAnimacion) {
       requestAnimationFrame(this.animate.bind(this));
     }
 
-    if (this.x++ < this.panAtX) {
-        this.ctx.fillRect(this.x, this.data[this.x], 1, 1);
-
-    } else {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        for (var xx = 0; xx < this.panAtX; xx++) {
-            var y = this.data[this.x - this.panAtX + xx];
-            this.ctx.fillRect(xx, y, 1, 1)
-        }
+    this.ctx.fillStyle="#FF0000";
+    if (this.x > this.data.length - 1) {
+        console.log("Me da que no hay datos");
+        return;
     }
+
+
+    var now = Date.now();
+    var transcurrido = now - this.then;
+    if (transcurrido > this.fpsIntervalo) {
+      console.log("Toca pinta");
+      if (this.x++ < this.panAtX) {
+          this.ctx.fillRect(this.x, this.data[this.x], 1, 1);
+
+      } else {
+          this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+          for (var xx = 0; xx < this.panAtX; xx++) {
+              var y = this.data[this.x - this.panAtX + xx];
+              this.ctx.fillRect(xx, y, 1, 1)
+          }
+      }
+      this.then = now - (transcurrido % this.fpsIntervalo);
+    }
+    else{
+      console.log("Ahora no toca pintar");
+    }
+   
 }
 
 
