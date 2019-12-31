2ac0c618d43fa4e5262c18dd91c2add0ceb9341a
José Manuel Castellano Domínguez <jmcastellano@jmcastellano.eu>
2019-12-31 19:55:34 +0100

    Fin version Sprint 6

--- Codigo/Health-Shirt/src/pages/constantes/constantes.ts
+++ Codigo/Health-Shirt/src/pages/constantes/constantes.ts
@@ -41,6 +41,12 @@ export class ConstantesPage {
   private pulsacionesmedias: any;
   private edamedias: any;
   private temperaturamedias: any;
+  private pulsacionesmin: any;
+  private edamin: any;
+  private temperaturamin: any;
+  private pulsacionesmax: any;
+  private edamax: any;
+  private temperaturamax: any;
 
   private datosecg: any;
   private datoseda: any;
@@ -105,17 +111,45 @@ export class ConstantesPage {
         //vamos a calcular las medias
         //la media de la temperatura es tan simple como sumar sus valores y dividirlos por el número del tamaño
         var mediat = 0;
+        var minencontrado = 100;
+        var maxencontrado = -100;
         for(var i=0; i< this.datostemperatura.length; i++){
           this.datostemperatura[i] = parseFloat(this.datostemperatura[i]);
           mediat+=this.datostemperatura[i];
+          if(minencontrado > this.datostemperatura[i]){
+            minencontrado = this.datostemperatura[i];
+          }
+          if(maxencontrado < this.datostemperatura[i]){
+            maxencontrado = this.datostemperatura[i];
+          }
+        }
+        this.temperaturamax = maxencontrado;
+        this.temperaturamin = minencontrado;
+        if(this.temperaturamin==100 || this.temperaturamax==-100){
+          this.temperaturamax = '';
+          this.temperaturamin = '';
         }
         this.temperaturamedias = Math.round( (mediat/this.datostemperatura.length) * 100) / 100
 
         //lo mismo con el eda
         mediat = 0;
+        minencontrado = 600;
+        maxencontrado = -100;
         for(i=0; i< this.datoseda.length; i++){
           this.datoseda[i] = parseInt(this.datoseda[i]);
           mediat+=this.datoseda[i];
+          if(minencontrado > this.datoseda[i]){
+            minencontrado = this.datoseda[i];
+          }
+          if(maxencontrado < this.datoseda[i]){
+            maxencontrado = this.datoseda[i];
+          }
+        }
+        this.edamax = maxencontrado;
+        this.edamin = minencontrado;
+        if(this.edamin==600 || this.edamax==-100){
+          this.edamax = '';
+          this.edamin = '';
         }
         this.edamedias = Math.round( (mediat/this.datoseda.length) * 100) / 100
 
@@ -124,15 +158,36 @@ export class ConstantesPage {
 
         mediat = 0;
         this.pulsacionesmedias = 0;
+        minencontrado = 200;
+        maxencontrado = -200;
+        var valorpulsaciones = 0;
         for(i=0; i< this.datosecg.length; i++){
+          //cada 1200 valores es un minuto
           this.datosecg[i] = parseInt(this.datosecg[i]);
           if(this.datosecg[i]>=550){
             this.pulsacionesmedias++;
+            valorpulsaciones++;
           }
-          else{
-            mediat=0;
+
+          if((i+1)%1200==0){
+            if(minencontrado > valorpulsaciones){
+              minencontrado = valorpulsaciones;
+            }
+            if(maxencontrado < valorpulsaciones){
+              maxencontrado = valorpulsaciones;
+            }
+            valorpulsaciones=0;
           }
+         
         }
+        this.pulsacionesmax = maxencontrado;
+        this.pulsacionesmin = minencontrado;
+        if(this.pulsacionesmin==200 || this.pulsacionesmax==-100){
+          this.pulsacionesmax = '';
+          this.pulsacionesmin = '';
+        }
+
+
         //ademas aqui tendrá que haber una accion para que se reinicien las animaciones y las graficas
         //se utilizará un flag
         this.valorecgcambiado = true;
@@ -170,6 +225,22 @@ export class ConstantesPage {
     this.pulsacionesmedias = pulsacionesmedias;
   }
 
+  public getPulsacionesmin(){
+    return this.pulsacionesmin;
+  }
+
+  public setPulsacionesmin(pulsacionesmin:any){
+    this.pulsacionesmin = pulsacionesmin;
+  }
+
+  public getPulsacionesmax(){
+    return this.pulsacionesmax;
+  }
+
+  public setPulsacionesmax(pulsacionesmax:any){
+    this.pulsacionesmax = pulsacionesmax;
+  }
+
   public getEdamedias(){
     return this.edamedias;
   }
@@ -178,12 +249,44 @@ export class ConstantesPage {
     this.edamedias = edamedias;
   }
 
+  public getEdamin(){
+    return this.edamin;
+  }
+
+  public setEdamin(edamin:any){
+    this.edamin = edamin;
+  }
+
+  public getEdamax(){
+    return this.edamax;
+  }
+
+  public setEdamax(edamax:any){
+    this.edamax = edamax;
+  }
+
   public getTemperaturamedias(){
     return this.temperaturamedias;
   }
 
   public setTemperaturamedias(temperaturamedias:any){
     this.temperaturamedias = temperaturamedias;
+  }
+
+  public getTemperaturamin(){
+    return this.temperaturamin;
+  }
+
+  public setTemperaturamin(temperaturamin:any){
+    this.temperaturamin = temperaturamin;
+  }
+
+  public getTemperaturamax(){
+    return this.temperaturamax;
+  }
+
+  public setTemperaturamax(temperaturamax:any){
+    this.temperaturamax = temperaturamax;
   }
 
   public getDatosecg(){
@@ -278,18 +381,48 @@ export class ConstantesPage {
 
         //vamos a calcular las medias
         //la media de la temperatura es tan simple como sumar sus valores y dividirlos por el número del tamaño
+        //vamos a calcular las medias
+        //la media de la temperatura es tan simple como sumar sus valores y dividirlos por el número del tamaño
         var mediat = 0;
+        var minencontrado = 100;
+        var maxencontrado = -100;
         for(var i=0; i< this.datostemperatura.length; i++){
           this.datostemperatura[i] = parseFloat(this.datostemperatura[i]);
           mediat+=this.datostemperatura[i];
+          if(minencontrado > this.datostemperatura[i]){
+            minencontrado = this.datostemperatura[i];
+          }
+          if(maxencontrado < this.datostemperatura[i]){
+            maxencontrado = this.datostemperatura[i];
+          }
+        }
+        this.temperaturamax = maxencontrado;
+        this.temperaturamin = minencontrado;
+        if(this.temperaturamin==100 || this.temperaturamax==-100){
+          this.temperaturamax = '';
+          this.temperaturamin = '';
         }
         this.temperaturamedias = Math.round( (mediat/this.datostemperatura.length) * 100) / 100
 
         //lo mismo con el eda
         mediat = 0;
+        minencontrado = 600;
+        maxencontrado = -100;
         for(i=0; i< this.datoseda.length; i++){
           this.datoseda[i] = parseInt(this.datoseda[i]);
           mediat+=this.datoseda[i];
+          if(minencontrado > this.datoseda[i]){
+            minencontrado = this.datoseda[i];
+          }
+          if(maxencontrado < this.datoseda[i]){
+            maxencontrado = this.datoseda[i];
+          }
+        }
+        this.edamax = maxencontrado;
+        this.edamin = minencontrado;
+        if(this.edamin==600 || this.edamax==-100){
+          this.edamax = '';
+          this.edamin = '';
         }
         this.edamedias = Math.round( (mediat/this.datoseda.length) * 100) / 100
 
@@ -298,20 +431,43 @@ export class ConstantesPage {
 
         mediat = 0;
         this.pulsacionesmedias = 0;
+        minencontrado = 200;
+        maxencontrado = -200;
+        var valorpulsaciones = 0;
         for(i=0; i< this.datosecg.length; i++){
+          //cada 1200 valores es un minuto
           this.datosecg[i] = parseInt(this.datosecg[i]);
           if(this.datosecg[i]>=550){
             this.pulsacionesmedias++;
+            valorpulsaciones++;
           }
-          else{
-            mediat=0;
+
+          if((i+1)%1200==0){
+            console.log(i);
+            console.log(valorpulsaciones);
+            if(minencontrado > valorpulsaciones){
+              minencontrado = valorpulsaciones;
+            }
+            if(maxencontrado < valorpulsaciones){
+              console.log("Cambiado");
+              maxencontrado = valorpulsaciones;
+            }
+            valorpulsaciones=0;
           }
+         
+        }
+        this.pulsacionesmax = maxencontrado;
+        this.pulsacionesmin = minencontrado;
+        if(this.pulsacionesmin==200 || this.pulsacionesmax==-100){
+          this.pulsacionesmax = '';
+          this.pulsacionesmin = '';
         }
 
         //y ahora a partir de la diferencia de minutos se divide
         
         var diffMs = (Date.parse(this.fechaHasta.toLocaleString()) - Date.parse(this.fechaDe.toLocaleString()));
         let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
+        console.log(diffMins);
         this.pulsacionesmedias = this.pulsacionesmedias / diffMins;
 
         //ademas aqui tendrá que haber una accion para que se reinicien las animaciones y las graficas
