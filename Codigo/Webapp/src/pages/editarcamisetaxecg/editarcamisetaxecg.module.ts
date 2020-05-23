e937f425ed533efbf9d49c554684cfba1ba00426
José Manuel Castellano Domínguez <jmcastellano@jmcastellano.eu>
2020-05-23 23:22:08 +0200

    Tabs personalizados

---
+++ Codigo/Webapp/src/pages/editarcamisetaxecg/editarcamisetaxecg.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EditarcamisetaxecgPage } from './editarcamisetaxecg';
+
+@NgModule({
+  declarations: [
+    EditarcamisetaxecgPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EditarcamisetaxecgPage),
+  ],
+})
+export class EditarcamisetaxecgPageModule {}
---
+++ Codigo/Webapp/src/pages/editarcamisetaxtemperatura/editarcamisetaxtemperatura.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EditarcamisetaxtemperaturaPage } from './editarcamisetaxtemperatura';
+
+@NgModule({
+  declarations: [
+    EditarcamisetaxtemperaturaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EditarcamisetaxtemperaturaPage),
+  ],
+})
+export class EditarcamisetaxtemperaturaPageModule {}
